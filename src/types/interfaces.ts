2d8dc32a4b3a46a0d07109869a3c58ad91c981ae
Alex-Benoy <20109551@mail.wit.ie>
2025-05-04 18:17:33 +0100

    Show movie review excerpts; Refactor Details page.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,57 +1,62 @@
 export interface BaseMovieProps {
-    title: string;
-    budget: number;
-    homepage: string | undefined;
-    id: number;
-    imdb_id: string;
-    original_language: string;
-    overview: string;
-    release_date: string;
-    vote_average: number;
-    popularity: number;
-    poster_path?: string;
-    tagline: string;
-    runtime: number;
-    revenue: number;
-    vote_count: number;
-    favourite?: boolean;
-    genre_ids?: number[];
-  }
+  title: string;
+  budget: number;
+  homepage: string | undefined;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  overview: string;
+  release_date: string;
+  vote_average: number;
+  popularity: number;
+  poster_path?: string;
+  tagline: string;
+  runtime: number;
+  revenue: number;
+  vote_count: number;
+  favourite?: boolean;
+  genre_ids?: number[];
+}
 
-export interface BaseMovieListProps { 
-    movies: BaseMovieProps[];
-    selectFavourite: (movieId: number) => void;
-}    
+export interface BaseMovieListProps {
+  movies: BaseMovieProps[];
+  selectFavourite: (movieId: number) => void;
+}
 
 export interface MovieDetailsProps extends BaseMovieProps {
-    genres: {
-      id: number;
-      name: string;
-    }[];
-    production_countries: {
-      iso_3166_1: string;
-      name: string;
-    }[];
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
 }
 
 export interface MovieImage {
-    file_path: string;
-    aspect_ratio?: number; //some props are optional...
-    height?: number;
-    iso_639_1?: string;
-    vote_average?: number;
-    vote_count?: number;
-    width?: number;
+  file_path: string;
+  aspect_ratio?: number; //some props are optional...
+  height?: number;
+  iso_639_1?: string;
+  vote_average?: number;
+  vote_count?: number;
+  width?: number;
 }
 
 export interface MoviePageProps {
-    movie: MovieDetailsProps;
-    images: MovieImage[];
+  movie: MovieDetailsProps;
+  images: MovieImage[];
 }
-  
+
 export type FilterOption = "title" | "genre";
 
 export interface MovieListPageTemplateProps extends BaseMovieListProps {
-    title: string;
-  }
-  
+  title: string;
+}
+
+export interface Review {
+  id: string;
+  content: string
+  author: string
+}
